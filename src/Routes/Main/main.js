4c9451eaafcae624ad4a6898dd0b3d7177460802
Jeonghun Sakong <jeonghunsakong@Jeonghunui-MacBookPro.local>
2020-02-08 00:43:00 +0900

    update profile

--- src/Routes/Main/main.js
+++ src/Routes/Main/main.js
@@ -7,7 +7,44 @@ const Container = styled.div`
     margin: 0 20px;
 `;
 
-const Main = () =>{
+const InputBox = styled.div`
+    border: 1px solid black;
+    padding: 20px;
+
+`;
+const InfoBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    padding: 20px;
+`;
+const Contents = styled.div`
+    display: inherit;
+    flex-direction: column;
+    margin-left: 30px;
+`;
+
+const Category = styled.span`
+    font-weight: 700;
+    font-size: 20px;
+    margin-right: 10px;
+    
+`;
+
+const Profile = styled.div`
+  background-image: ${props => `url(${props.background})`};
+  background-size: cover;
+  background-position: center center;
+  width: 185px;
+    height: 280px;
+`
+
+const Main = () => {
+    const [isInputBox, setIsInputBox] = useState(true);
+    const [profile, setProfile] = useState('');
+    const name = useInput('');
+    const age = useInput('');
+    const address = useInput('');
+    const phone = useInput('');
     const speaking = useInput(0);
     const writing = useInput(0);
     const listening = useInput(0);
@@ -29,17 +66,17 @@ const Main = () =>{
             xaxis: {
                 categories: ['Speaking', 'Writing', 'Listening', 'Reading', 'Attendance']
             },
-            yaxis:{
+            yaxis: {
                 min: 0,
-                max:500,
+                max: 500,
                 labels: {
-                  formatter: function(val, i) {
-                    if (i % 2 === 0) {
-                      return val
-                    } else {
-                      return ''
+                    formatter: function (val, i) {
+                        if (i % 2 === 0) {
+                            return val
+                        } else {
+                            return ''
+                        }
                     }
-                  }
                 }
             }
         }
@@ -48,18 +85,62 @@ const Main = () =>{
         console.log(speaking);
         setSeries([{
             name: 'Series',
-            data: [speaking.value,writing.value,listening.value,reading.value,attendance.value],
-        }])
+            data: [speaking.value, writing.value, listening.value, reading.value, attendance.value],
+        }]);
+        name.onClick();
+    };
+    const onImageChange = async (event) => {
+        console.log('image change');
+        if (event.target.files && event.target.files[0]){
+            setProfile(URL.createObjectURL(event.target.files[0]))
+        }
     }
 
     return (
         <Container>
-             <input placeholder={'speaking'} onChange={speaking.onChange}></input>
-            <input placeholder={'writing'} onChange={writing.onChange}></input>
-            <input placeholder={'listening'} onChange={listening.onChange}></input>
-            <input placeholder={'reading'} onChange={reading.onChange}></input>
-            <input placeholder={'attendance'} onChange={attendance.onChange}></input>
-            <button onClick={onClick}>클릭</button>
+            <InputBox>
+                프로필:<input accept="image/*" onChange={onImageChange} placeholder={"프로필"} type="file"></input>
+                <input style={{ display: 'block' }} placeholder={'이름'} onChange={name.onChange}></input>
+                <input style={{ display: 'block' }} placeholder={'나이'} onChange={age.onChange}></input>
+                <input style={{ display: 'block' }} placeholder={'주소'} onChange={address.onChange}></input>
+                <input style={{ display: 'block' }} placeholder={'연락처'} onChange={phone.onChange}></input>
+                <input placeholder={'speaking'} onChange={speaking.onChange}></input>
+                <input placeholder={'writing'} onChange={writing.onChange}></input>
+                <input placeholder={'listening'} onChange={listening.onChange}></input>
+                <input placeholder={'reading'} onChange={reading.onChange}></input>
+                <input placeholder={'attendance'} onChange={attendance.onChange}></input>
+                <button onClick={onClick}>클릭</button>
+            </InputBox>
+
+            <InfoBox>
+                <Profile background={profile} />
+                <Contents>
+                    <div style={{ marginBottom: '20px' }}>
+                        <Category>
+                            이름:
+                    </Category>
+                        {name.value}
+                    </div>
+                    <div style={{ marginBottom: '20px' }}>
+                        <Category>
+                            나이:
+                    </Category>
+                        {age.value}
+                    </div>
+                    <div style={{ marginBottom: '20px' }}>
+                        <Category>
+                            주소:
+                    </Category>
+                        {address.value}
+                    </div>
+                    <div style={{ marginBottom: '20px' }}>
+                        <Category>
+                            연락처:
+                    </Category>
+                        {phone.value}
+                </div>
+                </Contents>
+            </InfoBox>
             <Chart options={options} series={series} type="radar" height={350} />
         </Container>
     )
