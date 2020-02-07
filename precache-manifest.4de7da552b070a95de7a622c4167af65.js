0eabcf48e383d7b4ec6d3223c78151b357a3ddfb
Jeonghun Sakong <jeonghunsakong@Jeonghunui-MacBookPro.local>
2020-02-07 23:46:30 +0900

    Updates

---
+++ precache-manifest.4de7da552b070a95de7a622c4167af65.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59f415a33cc9ec993248a9ad5b195d10",
+    "url": "/potmaker/index.html"
+  },
+  {
+    "revision": "1e56e60687d377a8ec47",
+    "url": "/potmaker/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d15bd99b7423e2589d83",
+    "url": "/potmaker/static/js/2.57ec1e0b.chunk.js"
+  },
+  {
+    "revision": "10c3c4f73b0578a837b96ef723e22acf",
+    "url": "/potmaker/static/js/2.57ec1e0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e56e60687d377a8ec47",
+    "url": "/potmaker/static/js/main.0cf7f262.chunk.js"
+  },
+  {
+    "revision": "7f9a665fb8882666f0f7",
+    "url": "/potmaker/static/js/runtime-main.9fe1c7e5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/potmaker/static/media/logo.5d5d9eef.svg"
+  }
+]);
